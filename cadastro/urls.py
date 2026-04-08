1ca668acf1be01788f2208700ba674416d851e92
André Luferat <contato@luferat.net>
2026-04-07 21:44:28 -0300

    Ok

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,7 +1,10 @@
+# cadastro\urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Raiz (index.html)
     path('contato/', views.contato, name='contato'),
+    path('adicionar/', views.adicionar, name='adicionar'),
 ]
